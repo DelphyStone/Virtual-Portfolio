cce8d1b7d74a5e04270b04a89eeb3f2865cc4d43
Delphy Stone <tshegostone31@gmail.com>
2026-07-20 19:11:25 +0200

    Worked on the contact form sending me and email as well as the media queries

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ function toggleMenu()
     menu.classList.toggle("open");
     icon.classList.toggle("open");  
 }
+
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    const form = e.target;
+    const data = new FormData(form);
+
+    fetch(form.action, {
+        method: "POST",
+        body: data,
+        headers: { "Accept": "application/json" }
+    })
+    .then(response => {
+        if (response.ok) {
+            alert("Thanks! Your message has been sent.");
+            form.reset();
+        } else {
+            alert("Oops, something went wrong. Please try again or email me directly.");
+        }
+    })
+    .catch(() => {
+        alert("Oops, something went wrong. Please try again or email me directly.");
+    });
+});
