8ed64c98992037e81df3d2841c675af17234eff3
Delphy Stone <tshegostone31@gmail.com>
2026-07-21 15:48:39 +0200

    Polished the tech percentage bars.

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ function toggleMenu()
     icon.classList.toggle("open");  
 }
 
+/*CONTACT FORM*/
+
 document.getElementById("contact-form").addEventListener("submit", function(e) {
     e.preventDefault();
     const form = e.target;
@@ -28,3 +30,54 @@ document.getElementById("contact-form").addEventListener("submit", function(e) {
         alert("Oops, something went wrong. Please try again or email me directly.");
     });
 });
+
+/*ANIMATED TYPING*/
+
+const words = 
+[
+    "Software Developer",
+    "Web Developer",
+    "Problem Solver",
+    "IT Student",
+    "Python Programmer",
+    "Data Analyst"
+];
+
+const typingElement = document.querySelector(".typing");
+
+let wordIndex = 0;
+let charIndex = 0;
+let deleting = false;
+
+function type(){
+
+    const currentWord = words[wordIndex];
+
+    if(!deleting)
+    {
+        typingElement.textContent = currentWord.substring(0, charIndex + 1);
+        charIndex++;
+
+        if(charIndex === currentWord.length){
+            deleting = true;
+            setTimeout(type,1500);
+            return;
+        }
+
+    }
+    else
+    {
+
+        typingElement.textContent = currentWord.substring(0, charIndex - 1);
+        charIndex--;
+
+        if(charIndex === 0){
+            deleting = false;
+            wordIndex = (wordIndex + 1) % words.length;
+       }
+
+    }
+    setTimeout(type, deleting ? 60 : 120);
+}
+
+type();
